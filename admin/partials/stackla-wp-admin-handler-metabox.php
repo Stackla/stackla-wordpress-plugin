bb152bb85ff5e892a3c51faecd217a866f0bfc5d
George Zhao <zorjiz@gmail.com>
2015-12-03 14:21:51 +1100

    STTP-115 Code format

--- admin/partials/stackla-wp-admin-handler-metabox.php
+++ admin/partials/stackla-wp-admin-handler-metabox.php
@@ -1,82 +1,81 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('error_reporting', E_ALL);
-    require_once('../../../../../wp-load.php');
-    require_once('../../includes/vendor/autoload.php');
-    require_once('../../includes/class-stackla-wp-metabox.php');
-    require_once('../../includes/class-stackla-wp-sdk-wrapper.php');
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+require_once '../../../../../wp-load.php';
+require_once '../../includes/vendor/autoload.php';
+require_once '../../includes/class-stackla-wp-metabox.php';
+require_once '../../includes/class-stackla-wp-sdk-wrapper.php';
 
-    $post_id = $_POST['postId'];
-    $title = "WP-{$post_id}";
-    $terms = $_POST['terms'];
-    // $filters = $_POST['filters'];
-    $widget = $_POST['widget'];
-    $mediaType = isset($_POST['media_type']) ? $_POST['media_type'] : array();
+$post_id = $_POST['postId'];
+$title = "WP-{$post_id}";
+$terms = $_POST['terms'];
+// $filters = $_POST['filters'];
+$widget = $_POST['widget'];
+$mediaType = isset($_POST['media_type']) ? $_POST['media_type'] : array();
 
-    $settings = new Stackla_WP_Settings();
-    $validator = new Stackla_WP_Metabox_Validator($_POST);
-    $valid = $validator->validate();
+$settings = new Stackla_WP_Settings();
+$validator = new Stackla_WP_Metabox_Validator($_POST);
+$valid = $validator->validate();
 
-    $response = array('errors' => false , 'result' => '1');
-    if ($valid && !$validator->emptyTerm) {
-        $metabox = new Stackla_WP_Metabox($post_id);
-        $sdk = new Stackla_WP_SDK_Wrapper($post_id);
-        $oldData = $metabox->get_data();
-        $oldWidget = json_decode($oldData['widget'], 1);
+$response = array('errors' => false, 'result' => '1');
+if ($valid && !$validator->emptyTerm) {
+    $metabox = new Stackla_WP_Metabox($post_id);
+    $sdk = new Stackla_WP_SDK_Wrapper($post_id);
+    $oldData = $metabox->get_data();
+    $oldWidget = json_decode($oldData['widget'], 1);
 
-        if ($sdk === false)
-        {
-            exit();
-        }
-
-        $defaultTag = $sdk->push_tag($title);
-
-        if($defaultTag === false) {
-            echo $sdk->get_errors();
-            exit();
-        }
-        $metabox->set_stackla_wp_tag($defaultTag);
-
-        $defaultFilter = $sdk->prepareDefaultFilter($defaultTag, $title . " - ", $mediaType);
-
-        if($defaultFilter === false && $defaultTag === false) {
-            echo $sdk->get_errors();
-            exit();
-        }
-        $metabox->set_stackla_wp_defaultFilter($defaultFilter);
-        $metabox->set_stackla_wp_defaultFilterMedia($mediaType);
-
-        $stackla_terms = $sdk->push_terms($terms , $defaultTag, $title . " - ");
-        // $stackla_filters = $sdk->push_filters($filters , $defaultTag, $title . " - ");
-        $stackla_widget = $sdk->push_widget($title , $defaultFilter , $widget, $oldWidget);
-
-        $sdk_valid = $sdk->validate();
-
-        if($sdk_valid === false || $stackla_widget === false) {
-            echo $sdk->get_errors();
-            die();
-        }
-
-        $responseData[Stackla_WP_Metabox::$terms_meta_key] = $metabox->set_json($stackla_terms);
-        $responseData[Stackla_WP_Metabox::$filter_id_meta_key] = $defaultFilter->id;
-        $responseData[Stackla_WP_Metabox::$media_type_meta_key] = $metabox->set_json($mediaType);
-        $responseData[Stackla_WP_Metabox::$widget_meta_key] = $metabox->set_json(array(
-            'id' => $stackla_widget['id'],
-            'copyId' => $stackla_widget['copyId'],
-            'style' => $stackla_widget['style'],
-            'type' => $stackla_widget['type']
-        ));
-
-        $responseData[Stackla_WP_Metabox::$widget_embed_meta_key] = $stackla_widget['embed'];
-
-        $metabox->set_stackla_wp_terms($stackla_terms);
-        // $metabox->set_stackla_wp_filters($stackla_filters);
-        $metabox->set_stackla_wp_widget($stackla_widget);
-
-        $response['data'] = $responseData;
-    } else {
-        $response['errors'] = $validator->errors;
-        $response['result'] = 0;
+    if ($sdk === false) {
+        exit();
     }
-    echo json_encode($response);
+
+    $defaultTag = $sdk->push_tag($title);
+
+    if ($defaultTag === false) {
+        echo $sdk->get_errors();
+        exit();
+    }
+    $metabox->set_stackla_wp_tag($defaultTag);
+
+    $defaultFilter = $sdk->prepareDefaultFilter($defaultTag, $title . " - ", $mediaType);
+
+    if ($defaultFilter === false && $defaultTag === false) {
+        echo $sdk->get_errors();
+        exit();
+    }
+    $metabox->set_stackla_wp_defaultFilter($defaultFilter);
+    $metabox->set_stackla_wp_defaultFilterMedia($mediaType);
+
+    $stackla_terms = $sdk->push_terms($terms, $defaultTag, $title . " - ");
+    // $stackla_filters = $sdk->push_filters($filters , $defaultTag, $title . " - ");
+    $stackla_widget = $sdk->push_widget($title, $defaultFilter, $widget, $oldWidget);
+
+    $sdk_valid = $sdk->validate();
+
+    if ($sdk_valid === false || $stackla_widget === false) {
+        echo $sdk->get_errors();
+        die();
+    }
+
+    $responseData[Stackla_WP_Metabox::$terms_meta_key] = $metabox->set_json($stackla_terms);
+    $responseData[Stackla_WP_Metabox::$filter_id_meta_key] = $defaultFilter->id;
+    $responseData[Stackla_WP_Metabox::$media_type_meta_key] = $metabox->set_json($mediaType);
+    $responseData[Stackla_WP_Metabox::$widget_meta_key] = $metabox->set_json(array(
+        'id' => $stackla_widget['id'],
+        'copyId' => $stackla_widget['copyId'],
+        'style' => $stackla_widget['style'],
+        'type' => $stackla_widget['type']
+    ));
+
+    $responseData[Stackla_WP_Metabox::$widget_embed_meta_key] = $stackla_widget['embed'];
+
+    $metabox->set_stackla_wp_terms($stackla_terms);
+    // $metabox->set_stackla_wp_filters($stackla_filters);
+    $metabox->set_stackla_wp_widget($stackla_widget);
+
+    $response['data'] = $responseData;
+} else {
+    $response['errors'] = $validator->errors;
+    $response['result'] = 0;
+}
+echo json_encode($response);
 
