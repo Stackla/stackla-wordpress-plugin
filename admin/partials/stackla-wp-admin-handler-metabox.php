5ba83353c76372c0be7bd47a6359416851b2cdd5
George Zhao <zorjiz@gmail.com>
2015-12-04 14:20:29 +1100

    STTP-115 Fixed cannot save post when terms are empty

--- admin/partials/stackla-wp-admin-handler-metabox.php
+++ admin/partials/stackla-wp-admin-handler-metabox.php
@@ -20,56 +20,58 @@ $valid = $validator->validate();
 $response = array('errors' => false, 'result' => '1');
 try {
     if ($valid) {
-        $metabox = new Stackla_WP_Metabox($post_id);
-        $sdk = new Stackla_WP_SDK_Wrapper($post_id);
-        $oldData = $metabox->get_data();
-        $oldWidget = json_decode($oldData['widget'], 1);
+        if (!$validator->emptyTerm) {
+            $metabox = new Stackla_WP_Metabox($post_id);
+            $sdk = new Stackla_WP_SDK_Wrapper($post_id);
+            $oldData = $metabox->get_data();
+            $oldWidget = json_decode($oldData['widget'], 1);
 
-        $defaultTag = $sdk->push_tag($tagName);
+            $defaultTag = $sdk->push_tag($tagName);
 
-        if ($defaultTag === false) {
-            echo $sdk->get_errors();
-            exit();
+            if ($defaultTag === false) {
+                echo $sdk->get_errors();
+                exit();
+            }
+            $metabox->set_stackla_wp_tag($defaultTag);
+
+            $defaultFilter = $sdk->prepareDefaultFilter($defaultTag, $tagName . " - ", $mediaType);
+
+            if ($defaultFilter === false && $defaultTag === false) {
+                echo $sdk->get_errors();
+                exit();
+            }
+            $metabox->set_stackla_wp_defaultFilter($defaultFilter);
+            $metabox->set_stackla_wp_defaultFilterMedia($mediaType);
+
+            $stackla_terms = $sdk->push_terms($terms, $defaultTag, $tagName . " - ");
+            // $stackla_filters = $sdk->push_filters($filters , $defaultTag, $tagName . " - ");
+            $stackla_widget = $sdk->push_widget($tagName, $defaultFilter, $widget, $oldWidget);
+
+            $sdk_valid = $sdk->validate();
+
+            if ($sdk_valid === false || $stackla_widget === false) {
+                echo $sdk->get_errors();
+                die();
+            }
+
+            $responseData[Stackla_WP_Metabox::$terms_meta_key] = $metabox->set_json($stackla_terms);
+            $responseData[Stackla_WP_Metabox::$filter_id_meta_key] = $defaultFilter->id;
+            $responseData[Stackla_WP_Metabox::$media_type_meta_key] = $metabox->set_json($mediaType);
+            $responseData[Stackla_WP_Metabox::$widget_meta_key] = $metabox->set_json(array(
+                'id' => $stackla_widget['id'],
+                'copyId' => $stackla_widget['copyId'],
+                'style' => $stackla_widget['style'],
+                'type' => $stackla_widget['type']
+            ));
+
+            $responseData[Stackla_WP_Metabox::$widget_embed_meta_key] = $stackla_widget['embed'];
+
+            $metabox->set_stackla_wp_terms($stackla_terms);
+            // $metabox->set_stackla_wp_filters($stackla_filters);
+            $metabox->set_stackla_wp_widget($stackla_widget);
+
+            $response['data'] = $responseData;
         }
-        $metabox->set_stackla_wp_tag($defaultTag);
-
-        $defaultFilter = $sdk->prepareDefaultFilter($defaultTag, $tagName . " - ", $mediaType);
-
-        if ($defaultFilter === false && $defaultTag === false) {
-            echo $sdk->get_errors();
-            exit();
-        }
-        $metabox->set_stackla_wp_defaultFilter($defaultFilter);
-        $metabox->set_stackla_wp_defaultFilterMedia($mediaType);
-
-        $stackla_terms = $sdk->push_terms($terms, $defaultTag, $tagName . " - ");
-        // $stackla_filters = $sdk->push_filters($filters , $defaultTag, $tagName . " - ");
-        $stackla_widget = $sdk->push_widget($tagName, $defaultFilter, $widget, $oldWidget);
-
-        $sdk_valid = $sdk->validate();
-
-        if ($sdk_valid === false || $stackla_widget === false) {
-            echo $sdk->get_errors();
-            die();
-        }
-
-        $responseData[Stackla_WP_Metabox::$terms_meta_key] = $metabox->set_json($stackla_terms);
-        $responseData[Stackla_WP_Metabox::$filter_id_meta_key] = $defaultFilter->id;
-        $responseData[Stackla_WP_Metabox::$media_type_meta_key] = $metabox->set_json($mediaType);
-        $responseData[Stackla_WP_Metabox::$widget_meta_key] = $metabox->set_json(array(
-            'id' => $stackla_widget['id'],
-            'copyId' => $stackla_widget['copyId'],
-            'style' => $stackla_widget['style'],
-            'type' => $stackla_widget['type']
-        ));
-
-        $responseData[Stackla_WP_Metabox::$widget_embed_meta_key] = $stackla_widget['embed'];
-
-        $metabox->set_stackla_wp_terms($stackla_terms);
-        // $metabox->set_stackla_wp_filters($stackla_filters);
-        $metabox->set_stackla_wp_widget($stackla_widget);
-
-        $response['data'] = $responseData;
     } else {
         $response['errors'] = $validator->errors;
         $response['result'] = '0';
